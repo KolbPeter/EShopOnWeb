7ef1757a566b6bbc73f370e74c67cbb56af4d41a
Peter Kolb <Peter_Kolb@epam.com>
2023-02-16 15:11:51 +0100

    Set projects to work with dotnet 6 function

---
+++ src/OrderItemsReserver/Function1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace OrderItemsReserver
+{
+    public class Function1
+    {
+        private readonly ILogger _logger;
+
+        public Function1(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<Function1>();
+        }
+
+        [Function("Function1")]
+        public void Run([ServiceBusTrigger("neworder")] string myQueueItem)
+        {
+            _logger.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+        }
+    }
+}
